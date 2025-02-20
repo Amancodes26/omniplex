c8e17e77ceb2b87b64a30e7a85052c804106c32c
Amancodes26 <amansngh8586@gmail.com>
2025-02-20 21:28:06 +0530

    Add BackgroundRemoverPage component to handle background removal functionality

---
+++ src/app/Bgremover/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import BackgroundRemover from '@/components/Bgremover/Bgremover';
+
+export default function BackgroundRemoverPage() {
+  return (
+    <div className="min-h-screen bg-[#161616] text-white">
+      <BackgroundRemover />
+    </div>
+  );
+}
