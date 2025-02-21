45b3a389086dfd0b3c613029c6a9b5831c1b9fd6
Amancodes26 <amansngh8586@gmail.com>
2025-02-21 09:56:35 +0530

    fix depoly 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -13,6 +15,14 @@ const nextConfig = {
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
+      // Force Firebase Storage to use its browser build instead of the Node-specific build.
+      config.resolve.alias[
+        "@firebase/storage/dist/node-esm/index.node.esm.js"
+      ] = path.resolve(
+        __dirname,
+        "node_modules/@firebase/storage/dist/index.esm.js"
+      );
+      // Also, in case undici is still imported anywhere on the client, tell webpack to ignore it.
       config.resolve.fallback = {
         ...config.resolve.fallback,
         undici: false,
